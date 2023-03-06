c8b2673e2d458f463ed0bd0c04c8c9555394515f
Eduardo Ogasawara <eogasawara@eic.cefet-rj.br>
2023-03-05 22:08:28 -0300

    Update install_package.R

--- development/install_package.R
+++ development/install_package.R
@@ -1,8 +1,8 @@
 library(devtools)
 
 devtools::install_github("cefet-rj-dal/dal", force=TRUE)
-
 library(dal)
 
 devtools::install_github("cefet-rj-dal/dal", force=TRUE, dependencies = TRUE, build_vignettes = TRUE)
+library(dal)
 utils::browseVignettes()
