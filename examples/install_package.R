7dda03d90208d154da2db0a0115b77648d8681aa
Eduardo Ogasawara <eogasawara@eic.cefet-rj.br>
2023-04-07 12:37:13 -0300

    Update

--- examples/install_package.R
+++ examples/install_package.R
@@ -1,8 +1,8 @@
 library(devtools)
 
-devtools::install_github("cefet-rj-dal/dal", force=TRUE)
-library(dal)
+devtools::install_github("cefet-rj-dal/daLtoolbox", force=TRUE)
+library(daLtoolbox)
 
-devtools::install_github("cefet-rj-dal/dal", force=TRUE, dep = FALSE, build_vignettes = TRUE)
-library(dal)
+devtools::install_github("cefet-rj-dal/daLtoolbox", force=TRUE, dep = FALSE, build_vignettes = TRUE)
+library(daLtoolbox)
 utils::browseVignettes()
