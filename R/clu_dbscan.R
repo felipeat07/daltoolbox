805aa47c55d82d25869ab91734f8cfe19d2ae5c1
eogasawara <eogasawara@eic.cefet-rj.br>
2023-06-13 21:23:45 -0300

    clustering

--- R/clu_dbscan.R
+++ R/clu_dbscan.R
@@ -12,14 +12,14 @@
 #'@details
 #'
 #'@param eps
-#'@param MinPts
+#'@param minPts
 #'@return
 #'@examples
 #'@export
-cluster_dbscan <- function(eps, MinPts) {
+cluster_dbscan <- function(eps, minPts) {
   obj <- clustering()
   obj$eps <- eps
-  obj$MinPts <- MinPts
+  obj$minPts <- minPts
 
   class(obj) <- append("cluster_dbscan", class(obj))
   return(obj)
@@ -28,7 +28,7 @@ cluster_dbscan <- function(eps, MinPts) {
 #'@import dbscan
 #'@export
 cluster.cluster_dbscan <- function(obj, data) {
-  t <- sort(dbscan::kNNdist(data, k = obj$MinPts))
+  t <- sort(dbscan::kNNdist(data, k = obj$minPts))
   y <- t
   myfit <- fit_curvature_max()
   res <- transform(myfit, y)
@@ -40,7 +40,7 @@ cluster.cluster_dbscan <- function(obj, data) {
 #'@import dbscan
 #'@export
 cluster.cluster_dbscan <- function(obj, data) {
-  cluster <- dbscan::dbscan(data, eps = obj$eps, MinPts = obj$MinPts)
+  cluster <- dbscan::dbscan(data, eps = obj$eps, minPts = obj$minPts)
   cluster <- cluster$cluster
   attr(cluster, "metric") <- 0
   return(cluster)
