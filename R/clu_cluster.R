cb632fd0b40b147bf1338198c1d1219bda0f0aac
eogasawara <eogasawara@eic.cefet-rj.br>
2023-06-18 01:30:50 -0300

    update n usage

--- R/clu_cluster.R
+++ R/clu_cluster.R
@@ -53,7 +53,7 @@ cluster <- function(obj, ...) {
 #'@param ... optional arguments.
 #'@return Computed metrics
 #'@examples trans <- dal_transform()
-#'@importFrom dplyr filter summarise group_by
+#'@importFrom dplyr filter summarise group_by n
 #'@export
 evaluate.clustering <- function(obj, cluster, attribute, ...) {
   x <- y <- e <- qtd <- n <- 0
@@ -64,8 +64,8 @@ evaluate.clustering <- function(obj, cluster, attribute, ...) {
     options(dplyr.summarise.inform = FALSE)
 
     dataset <- data.frame(x = obj$data, y = obj$attribute)
-    tbl <- dataset |> dplyr::group_by(x, y) |> dplyr::summarise(qtd=n())
-    tbs <- dataset |> dplyr::group_by(x) |> dplyr::summarise(t=n())
+    tbl <- dataset |> dplyr::group_by(x, y) |> dplyr::summarise(qtd=dplyr::n())
+    tbs <- dataset |> dplyr::group_by(x) |> dplyr::summarise(t=dplyr::n())
     tbl <- base::merge(x=tbl, y=tbs, by.x="x", by.y="x")
     tbl$e <- -(tbl$qtd/tbl$t)*log(tbl$qtd/tbl$t,2)
     tbl <- tbl |> dplyr::group_by(x) |> dplyr::summarise(ce=sum(e), qtd=sum(qtd))
