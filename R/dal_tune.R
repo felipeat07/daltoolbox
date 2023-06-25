93b3e38185f1c8869ec42385a3a5c2f1ec3315ec
eogasawara <eogasawara@eic.cefet-rj.br>
2023-06-25 04:12:37 -0300

    update

--- R/dal_tune.R
+++ R/dal_tune.R
@@ -1,3 +1,9 @@
+#'@title select hyper parameters
+#'@description select hyper parameters
+#'@param obj object
+#'@param hyperparameters data set with hyper parameters.
+#'@return index of hyperparameter
+#'@examples trans <- dal_transform()
 #'@export
 select_hyper <- function(obj, hyperparameters) {
   UseMethod("select_hyper")
